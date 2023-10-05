8832cdc94d077630f32657a68b0d1e7ef0753eb4
Nikita <109899745+vvetta@users.noreply.github.com>
2023-10-05 17:09:37 +0300

    start_refactoring

--- code/parser_logic.py
+++ code/parser_logic.py
@@ -1,20 +1,57 @@
 import time
 import requests
+from typing import NamedTuple
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from settings import PATH_TO_WEBDRIVER, PARS_URL
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
-def _get_table_rows(driver):
+class TableRowsResult(NamedTuple):
+    result: WebElement
+    number_of_table_rows: int
 
-    result = driver.find_element(By.CLASS_NAME, "tpBody")
-    number = driver.find_elements(By.CLASS_NAME, "tpRow")
 
-    return result, len(number)
-    
+def init_parser(url: str, options: Options=None) -> WebDriver:
+    """
+    En: Initialization of the parser. Returns an instance of the WebDriver class.
+
+    Ru: Инициализация парсера. Возвращает экземпляр класса WebDriver.
+    """
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url=url)
+
+    return driver
+
+
+def _get_table_and_table_rows(driver: WebDriver, \
+                              class_of_table: str, class_of_rows: str) -> TableRowsResult:
+    """
+    En: Gets an instance of the WebDriver class, 
+    Css table class and Css row class, returns the table and the number of rows.
+
+    Ru: Принимает экземпляр класса WebDriver, 
+    Css класс таблицы и Css класс строки, возвращает таблицу и количество строк.
+    """
+
+    table = driver.find_element(By.CLASS_NAME, class_of_table)
+    number_of_rows = driver.find_elements(By.CLASS_NAME, class_of_rows)
+
+    return table, len(number_of_rows)
+
+
+def _format_result_data(data: list) -> dict:
+    """
+    En: Gets a date as a list, formats it, and returns a dictionary.
+
+    Ru: Принимает дату в виде списка, форматирует и возвращает словарь.
+    """
+    pass
 
 
 def parser():
