36f901eb778e4e7e3bf4a3a5b27ba26a70844bdd
Nikita <109899745+vvetta@users.noreply.github.com>
2023-10-07 12:10:06 +0300

    add try structure

--- code/main.py
+++ code/main.py
@@ -3,7 +3,12 @@ from storage_logic import save_file
 
 
 def main():
-    save_file(parser())
+    try:
+        save_file(parser())
+    except Exception as ex:
+        print(ex)
+    finally:
+        print("\nПрограмма отработала!\n")
 
 
 if __name__ == "__main__":
--- code/storage_logic.py
+++ code/storage_logic.py
@@ -5,3 +5,5 @@ def save_file(data) -> None:
     with open("data.txt", "w") as f:
         f.writelines(f"{element}\n" for element in data)
         f.close()
+    
+    print("\nФайл был успешно сохранён!\n")
